a43d46610fd0e98f9df43e639d232f3f0bad5103
tamzi <tamrefrank@gmail.com>
2021-09-02 18:42:28 +0300

    Initial commit

---
+++ app/src/main/java/com/tamzi/colorpallete/ui/theme/Color.kt
@@ -0,0 +1,53 @@
+/*
+ * Compiled by Tamzi
+ *
+ * You may use this in you compose android projectsThey are arraged in sets with already defined color codes.
+ * taken from uiehues.com
+ */
+package com.tamzi.colorpallete.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/*Default from compose*/
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+/*
+ * Set one, from theme
+ *
+ */
+val white = Color(0xFFFFFFFF)
+val bluish = Color(0xFF5944BE)
+val darkBluish = Color(0xFF312651)
+val black = Color(0xFF131313)
+val reddish = Color(0xFFFF4343)
+val warmRed = Color(0xFFFF7754)
+val limeGreenish = Color(0xFF4ABB00)
+val gray = Color(0xFFEFEFEF)
+val deepGray = Color(0xFFE6E4E6)
+val darkGray = Color( 0xFF83829A)
+val lightGray = Color(0xFFF6F6F6)
+val buttonRed = Color(0xFFFF7777CC)
+
+
+/*
+ * Set two
+ *
+ */
+val amber = Color(0xFFFCC775)
+val lighterBluish = Color(0xFF11D5E7)
+val lightBluish = Color(0xFF1EAAE5)
+val blue = Color(0xFF2A2F8F)
+
+
+/*
+ * Set 3
+ *
+ */
+val indigo = Color(0xFF452344)
+val reddisher = Color(0xFFE96B54)
+val amberish = Color(0xFFFEC253)
+val warmishGreen = Color(0xFF47B39C)
