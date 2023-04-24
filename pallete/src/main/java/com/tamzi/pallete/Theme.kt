7ecc66867a4d96af939a10b22016a966c7720486
tamzi <tamrefrank@gmail.com>
2023-04-24 07:39:01 +0200

    refix pallete

---
+++ pallete/src/main/java/com/tamzi/pallete/Theme.kt
@@ -0,0 +1,77 @@
+package com.tamzi.pallete
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import com.tamzi.pallete.atoms.Type
+import com.tamzi.pallete.atoms.amber
+import com.tamzi.pallete.atoms.black
+import com.tamzi.pallete.atoms.blue
+import com.tamzi.pallete.atoms.indigo
+import com.tamzi.pallete.atoms.warmishGreen
+import com.tamzi.pallete.atoms.white
+
+val DarkColorPalette = darkColorScheme(
+    primary = amber,
+    secondary = blue,
+    background = black,
+)
+
+val LightColorPalette = lightColorScheme(
+    primary = indigo,
+    secondary = warmishGreen,
+    background = white,
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+
+@Composable
+fun ColorPalleteTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorPallete = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorPallete.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorPallete,
+        typography = Type,
+        content = content
+    )
+}
