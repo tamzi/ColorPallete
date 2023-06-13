0c7e1c379d84222b1b981fc21aa91a19d4b6faf0
tamzi <tamrefrank@gmail.com>
2023-06-13 23:37:34 +0200

    updates dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,12 +55,12 @@ android {
 
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2023.04.01")
+    val composeBom = platform("androidx.compose:compose-bom:2023.05.01")
     implementation(composeBom)
 
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
 
     implementation("androidx.compose.ui:ui:1.4.3")
     implementation("androidx.compose.ui:ui-graphics:1.4.3")
